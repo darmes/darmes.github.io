a909b5ef4e0afde43d28fa4b0ca68d2654163284
David <darmes@users.noreply.github.com>
2025-07-17 14:35:32 -0400

    import primevue

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,19 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Material from '@primeuix/themes/material'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  // Default theme configuration
+  theme: {
+    preset: Material,
+  },
+})
 
 // mount after the initial navigation is ready
 // await router.isReady()
