76c2d3208d0283497bb042d6d8a7386469cc1d22
David <darmes@users.noreply.github.com>
2025-08-14 10:13:45 -0400

    swap about,home

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,24 +1,80 @@
 <template>
-  <div>
-    <p>Hi! Welcome to my site!</p>
-    <p>My name's David and I'm a senior software engineer, a proven leader, and a gamer.</p>
-    <p>
-      I've been writing code for over 10 years. I have a wide breadth of technical knowledge,
-      including artificial intelligence, frontend frameworks, backend web services, microservices,
-      relational, and non-relational databases.
-    </p>
-    <p>
-      I'm a veteran with combat tested leadership skills. I'm trained to educate, mentor, and build
-      up both individuals and teams. I'm a strong communicator with proficiency explaining technical
-      topics.
-    </p>
-    <p>
-      Some of my favorite games are Shadow of the Colossus, Mass Effect, and Civ 5 - just to name a
-      few. I'm an avid gamer and enjoy nearly every genre: FPS, Action, Platformer, Strategy, RPG,
-      Roguelike, Sci-Fi, and Simulation. I enjoy making games as much as I enjoy playing them, and
-      hope to craft memorable experiences.
-    </p>
+  <div class="HomeView">
+    <div class="AvatarContainer">
+      <Avatar image="/profile.webp" shape="circle" class="Avatar" />
+    </div>
+    <div class="Name">David Armes</div>
+    <div class="Title">Senior Software Engineer</div>
+    <div class="Intro">
+      <p>My name's David and I'm a senior software engineer, a proven leader, and a gamer.</p>
+      <p>
+        I've been writing code for over 10 years. I have a wide breadth of technical knowledge,
+        including artificial intelligence, frontend frameworks, backend web services, microservices,
+        relational, and non-relational databases.
+      </p>
+      <p>
+        I'm a veteran with combat tested leadership skills. I'm trained to educate, mentor, and
+        build up both individuals and teams. I'm a strong communicator with proficiency explaining
+        technical topics.
+      </p>
+      <p>
+        Some of my favorite games are Shadow of the Colossus, Mass Effect, and Civ 5 - just to name
+        a few. I'm an avid gamer and enjoy nearly every genre: FPS, Action, Platformer, Strategy,
+        RPG, Roguelike, Sci-Fi, and Simulation. I enjoy making games as much as I enjoy playing
+        them, and hope to craft memorable experiences.
+      </p>
+    </div>
   </div>
 </template>
 
-<style></style>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+import Avatar from 'primevue/avatar'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    Avatar,
+  },
+  setup() {
+    return {
+      windowSize: useWindowSize(),
+    }
+  },
+  computed: {
+    isMobile() {
+      return this.windowSize.width.value <= 768
+    },
+  },
+})
+</script>
+
+<style>
+.Name {
+  display: block;
+  text-align: center;
+  font-size: xx-large;
+  font-weight: bold;
+}
+.Title {
+  display: block;
+  text-align: center;
+  font-size: large;
+}
+.Intro {
+  text-align: justify;
+  margin: 1rem auto;
+  max-width: 60rem;
+  line-height: 1.5;
+}
+.Intro p {
+  margin-bottom: 2rem;
+}
+div .Avatar {
+  width: 20rem;
+  height: 20rem;
+  margin: 0 auto;
+  display: block;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,23 +6,23 @@
     <div class="Name">David Armes</div>
     <div class="Title">Senior Software Engineer</div>
     <div class="Intro">
-      <p>My name's David and I'm a senior software engineer, a proven leader, and a gamer.</p>
       <p>
-        I've been writing code for over 10 years. I have a wide breadth of technical knowledge,
-        including artificial intelligence, frontend frameworks, backend web services, microservices,
-        relational, and non-relational databases.
+        Accomplished leader with over six years’ experience building, mentoring, and training teams.
+        Wide breadth of technical knowledge, including artificial intelligence, frontend frameworks,
+        backend web services, microservices, relational, and non-relational databases. Strong
+        communicator with proficiency explaining technical topics.
       </p>
-      <p>
-        I'm a veteran with combat tested leadership skills. I'm trained to educate, mentor, and
-        build up both individuals and teams. I'm a strong communicator with proficiency explaining
-        technical topics.
-      </p>
-      <p>
-        Some of my favorite games are Shadow of the Colossus, Mass Effect, and Civ 5 - just to name
-        a few. I'm an avid gamer and enjoy nearly every genre: FPS, Action, Platformer, Strategy,
-        RPG, Roguelike, Sci-Fi, and Simulation. I enjoy making games as much as I enjoy playing
-        them, and hope to craft memorable experiences.
-      </p>
+    </div>
+    <div class="Links">
+      <pv-button asChild v-slot="slotProps">
+        <RouterLink to="/Skills" :class="slotProps.class">Skills</RouterLink>
+      </pv-button>
+      <pv-button asChild v-slot="slotProps">
+        <RouterLink to="/Experience" :class="slotProps.class">Experience</RouterLink>
+      </pv-button>
+      <pv-button asChild v-slot="slotProps">
+        <RouterLink to="/projects" :class="slotProps.class">Projects</RouterLink>
+      </pv-button>
     </div>
   </div>
 </template>
@@ -31,11 +31,13 @@
 import { defineComponent } from 'vue'
 import { useWindowSize } from '@vueuse/core'
 import Avatar from 'primevue/avatar'
+import pvButton from 'primevue/button'
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     Avatar,
+    pvButton,
   },
   setup() {
     return {
@@ -70,6 +72,13 @@ export default defineComponent({
 }
 .Intro p {
   margin-bottom: 2rem;
+}
+.Links {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  column-gap: 1rem;
+  row-gap: 1rem;
 }
 div .Avatar {
   width: 20rem;
