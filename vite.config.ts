ff7491aa5dce56c1d3020f5a22fb98b4f46e78b4
David <darmes@users.noreply.github.com>
2025-08-28 12:59:24 -0400

    create hidden page

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     target: 'esnext', //browsers can handle the latest ES features
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        hidden: resolve(__dirname, 'hidden/index.html'),
+      },
+    },
   },
   plugins: [vue(), vueDevTools()],
   resolve: {
